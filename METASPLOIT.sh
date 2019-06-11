5dcc1e6382acb6189f8e12cbc0d38acc49c9efd1
CYBER SEC <51013762+CYBERSECARYAN@users.noreply.github.com>
2019-06-11 14:54:23 +0530

    Add files via upload

---
+++ METASPLOIT.sh
@@ -0,0 +1,92 @@
+#!/termux/data/data/bin/bash
+# script by ARYAN from [ CYBER SEC ] youtube channel
+apt-get update -y
+apt-get upgrade -y
+pkg install python2 -y
+pkg install python -y
+pip install lolcat
+clear
+echo $red 
+echo ' 
+         (\____/)
+          (_oo_)
+            (O)
+          __||__    \)
+       []/______\[] /
+       / \______/ \/
+      /    /__\
+     (\   /____\
+
+/....TuRn On yOur IntErneT coNnecTion....\' | lolcat
+echo " "
+echo "### This Script is written by CYBER SEC ###" 
+echo " "
+echo "----------- Fetching.../// ---------️----"
+echo " "
+pkg install wget -y
+pkg install openssh -y
+pkg install ruby -y
+pkg install unstable-repo -y
+pkg install metasploit -y
+
+ls
+
+cd ..
+
+ls
+
+cd usr
+
+ls
+
+cd opt
+
+ls
+
+mv metasploit-framework $HOME
+
+ls
+
+cd /data/data/com.termux/files/home
+
+ls
+
+cd metasploit-framework
+
+ls
+
+wget https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt
+
+bash fix-ruby-bigdecimal.sh.txt
+
+echo " "
+echo "############# Completed ################"                                                                                                                       
+echo " "
+echo "If You Are Connected To Internet Then 
+you have downloaded metasploit successfully. 
+Subscribe us on youtube [ CYBER SEC ]"
+echo " "
+echo " (x ...BE A WHITE HAT HACKER... x) "
+echo " "
+echo '
+          \__/          Bye Bye guys...
+         `(  `^=_ p _###_
+          c   /  )  |   /
+   _____- //^---~  _c  3
+ /  ----^\ /^_\   / --,-
+(   |  |  O_| \\_/  ,/
+|   |  | / \|  `-- /
+(((G   |-----|
+      //-----\\
+     //       \\
+   /   |     |  ^|
+   |   |     |   |
+   |____|    |____|
+  /______)   (_____\' | lolcat
+echo " "
+echo "Type msfconsole to start ./msfconsole..."
+echo " "
+
+./msfconsole
+
+exit
